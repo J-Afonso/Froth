855d8839a1184d261e111811de690180deae6ece
Joao Afonso <joao.afonso@bab-oon.com>
2017-09-05 23:38:26 +0100

    Add bezier stubs

---
+++ bezier/src/cubic_bezier.cpp
@@ -0,0 +1,18 @@
+#include "cubic_bezier.h"
+
+namespace bezier {
+  
+  CubicBezier::CubicBezier(const Vector3f& point_a, const Vector3f& point_b,
+    const Vector3f& handle_a, const Vector3f& handle_b) {
+    _point_a = point_a;
+    _point_b = point_a;
+    _handle_a = handle_a;
+    _handle_b = handle_b;
+  }
+
+  Vector3f  CubicBezier::Interpolate(const float t) {
+    return Eigen::Vector3f(10, 20, 30);
+  }
+
+} // namespace bezier
+
