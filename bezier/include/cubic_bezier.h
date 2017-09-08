463577251574127dbe1aca35feff64a5c471f6cc
Joao Afonso <joao.afonso@bab-oon.com>
2017-09-08 11:17:32 +0100

    Added the bezier math

--- bezier/include/cubic_bezier.h
+++ bezier/include/cubic_bezier.h
@@ -1,12 +1,12 @@
 #ifndef BEZIER_CUBIC_BEZIER_H
 #define BEZIER_CUBIC_BEZIER_H
-//#include <Eigen/Core>
+#include <Eigen/Core>
 
-//using Eigen::Vector3f;
+using Eigen::Vector3f;
 
 namespace bezier {
 	class CubicBezier {
-	/*  public:
+	  public:
       CubicBezier(const Vector3f& point_a, const Vector3f& point_b,
         const Vector3f& handle_a, const Vector3f& handle_b);		  
       Vector3f Interpolate(const float t);
@@ -14,7 +14,7 @@ namespace bezier {
 		  Vector3f _point_a;
 		  Vector3f _point_b;
 		  Vector3f _handle_a;
-		  Vector3f _handle_b;*/
+		  Vector3f _handle_b;
 	  };
 } // namespace bezier
 #endif //BEZIER_CUBIC_BEZIER_H
--- bezier/src/cubic_bezier.cpp
+++ bezier/src/cubic_bezier.cpp
@@ -1,18 +1,22 @@
 #include "cubic_bezier.h"
 
+#include <math.h> 
+
 namespace bezier {
   
-  /*CubicBezier::CubicBezier(const Vector3f& point_a, const Vector3f& point_b,
-    const Vector3f& handle_a, const Vector3f& handle_b) {
-    _point_a = point_a;
-    _point_b = point_a;
-    _handle_a = handle_a;
-    _handle_b = handle_b;
+  CubicBezier::CubicBezier(const Vector3f& point_a, const Vector3f& point_b,
+    const Vector3f& handle_a, const Vector3f& handle_b) : _point_a(point_a),
+    _point_b(point_b), _handle_a (handle_a), _handle_b(handle_b){
   }
 
-  Vector3f  CubicBezier::Interpolate(const float t) {
-    return Eigen::Vector3f(10, 20, 30);
-  }*/
+  Vector3f CubicBezier::Interpolate(const float t) {
+    float inverted_t = (1 - t);
+    Vector3f p0 = pow(inverted_t, 3) * _point_a;
+    Vector3f p1 = 3 * pow(inverted_t, 2) * t * _handle_a;
+    Vector3f p2 = 3 * inverted_t * pow(t, 2) * _handle_b;
+    Vector3f p3 = pow(t, 3) * _point_b;
+    return p0 + p1 + p2 + p3;
+  }
 
 } // namespace bezier
 
